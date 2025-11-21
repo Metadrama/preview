184e1b7556bab52f2ead7eb0096066879e95fbbe
metadrama <aminnur.zainol@s.unikl.edu.my>
2025-11-21 16:35:13 +0800

    removed theme toggle label for more compact button

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -53,7 +53,6 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12.79A9 9 0 1111.21 3a7 7 0 109.79 9.79z" />
                     </svg>
                 </span>
-                <span class="text-sm font-medium theme-strong-text" data-theme-label>Dark</span>
             </button>
 
             <!-- Settings -->
